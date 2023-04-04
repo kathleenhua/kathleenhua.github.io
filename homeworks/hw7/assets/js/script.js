43b1943bc5d712688d8ec5e409a3bd365276dbc7
Kathleen Hua <kathleenhua@berkeley.edu>
2023-04-03 18:01:54 -0700

    finished hw7

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,24 @@
+function averageThreeNumbers(a, b, c) {
+    let sum = a + b + c;
+    let avg = sum/3;
+    return avg;
+}
+
+function createSentence(num, noun) {
+    // return "On average, a Berkeley student has " + num + " " + noun + "s";
+    return `On average, a Berkeley student has ${num} ${noun}s`;
+}
+
+function getRandomNum(max) {
+    return Math.random() * (max - 0) + 0;
+}
+
+let x = getRandomNum(20);
+let y = getRandomNum(10);
+let z = getRandomNum(13);
+
+let avg = averageThreeNumbers(x, y, z);
+
+let sentence = createSentence(avg, "panda");
+
+console.log(sentence);
